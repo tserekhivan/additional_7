d119f192ffb4e29041893e262c6934cdd91d665c
Tserekh Ivan <tserekh.ivan@gmail.com>
2018-09-19 03:18:53 +0300

    19 Sep 2018

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  if (solve(matrix)) return matrix;
+  else return 0;
+}
+
+function solve(matrix) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (matrix[i][j] != 0) {
+        continue;
+      }
+      for (let key = 1; key <= 9; key++) {
+        if (findPossibleValue(matrix, i, j, key) == true) {
+          matrix[i][j] = key;
+          if (solve(matrix)) {
+            return true;
+          }
+          matrix[i][j] = 0;
+        }
+      }
+      return false;
+    }
+  }
+  return true;
+}
+
+function findPossibleValue(matrix, rowIndex, columnIndex, key)
+{
+  if (!findRowValue(matrix, rowIndex, columnIndex, key)) return false;
+  if (!findColumnValue(matrix, rowIndex, columnIndex, key)) return false;
+  if (!findBlockValue(matrix, rowIndex, columnIndex, key)) return false;
+  return true;
+}
+
+function findRowValue(matrix, rowIndex, columnIndex, key) {
+  for (let j = 0; j < 9; j++) {
+    if (j != columnIndex && matrix[rowIndex][j] == key) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findColumnValue(matrix, rowIndex, columnIndex, key) {
+  for (let i = 0; i < 9; i++) {
+    if (i != rowIndex && matrix[i][columnIndex] == key) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findBlockValue(matrix, rowIndex, columnIndex, key) {
+  let startRow = Math.floor((rowIndex / 3)) * 3;
+  let startColumn = Math.floor((columnIndex / 3)) * 3;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (i != rowIndex && j != columnIndex && matrix[startRow + i][startColumn + j] == key) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
